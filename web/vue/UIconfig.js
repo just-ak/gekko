7af1f5cbcba45f4b385fe57e0f12018d9a2ff892
just-ak <andrew@akfdev.com>
2017-11-03 10:58:46 +0700

    UIConfig put into home/.gekko/UIConfig.js.json

--- web/vue/UIconfig.js
+++ web/vue/UIconfig.js
@@ -3,7 +3,7 @@
 
 // see https://github.com/askmike/gekko/blob/stable/docs/installing_gekko_on_a_server.md
 
-const CONFIG = {
+var  cfg = {
   headless: false,
   api: {
     host: '127.0.0.1',
@@ -18,6 +18,29 @@ const CONFIG = {
   },
   adapter: 'sqlite'
 }
+
+// Allows Movement of UI Config to local environment to allow customisation.
+// without worrying about git pull overwriting your settings.
+//           
+// TODO : Merge & Writeback, to include new settings when introduced.
+//
+var path = require('path');
+var fs = require('fs');
+var scriptName = path.basename(__filename);
+var dir = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME']  + '/.gekko';
+var filename = dir + '/'+scriptName+'.json';
+if (!fs.existsSync(dir)){
+    fs.mkdirSync(dir);
+}
+
+if (fs.existsSync(filename)) { 
+  cfg  = JSON.parse(fs.readFileSync(filename, 'utf8'));
+} else {
+  fs.writeFile(filename, JSON.stringify(cfg, null, 2) , 'utf-8');
+}
+console.log(JSON.stringify(cfg,null,4));
+
+const CONFIG = cfg;
 
 if(typeof window === 'undefined')
   module.exports = CONFIG;
